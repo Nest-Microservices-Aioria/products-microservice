d5999a51807b772e01b83fbe57a8f53b8ea4708a
Lenar Lima <lenar.toledo@gmail.com>
2024-06-26 01:45:23 -0400

    products-ms with TCP communication

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -48,7 +49,12 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       }
     })
 
-    if(!product) throw new NotFoundException(`Product with id #${id} not found`);
+    if(!product) {
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
 
     return {
       ...product
@@ -82,5 +88,27 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     return product;
+  }
+
+  async validateProducts(ids: number[]) {
+    
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids
+        }
+      }
+    });
+
+    if ( products.length !== ids.length ) {
+      throw new RpcException({
+        message: 'Some products were not found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
   }
 }
